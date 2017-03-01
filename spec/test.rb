e7ea46f0b46755e831980709ee8b2f67193a321f
elifoster <elifosterwy@gmail.com>
2017-02-28 18:16:03 -0800

    :white_check_mark: Ensure self is modified in prepend_capped tests

--- spec/test.rb
+++ spec/test.rb
@@ -6,13 +6,21 @@ using ArrayUtility
 describe 'ArrayUtility' do
   it 'gets the next value in an array' do
     assert_equal(2, [1, 2, 3].next(1))
-    assert_equal(nil, [1, 2, 3].next(3))
+    assert_nil([1, 2, 3].next(3))
   end
 
   it 'prepends a value with a maximum number of values in the array' do
-    assert_equal([4, 1, 2], [1, 2, 3].prepend_capped(4, 3))
-    assert_equal([4, 1, 2], [1, 2].prepend_capped(4, 3))
-    assert_equal([4], [1, 2].prepend_capped(4, 1))
+    value = [1, 2, 3]
+    assert_equal([4, 1, 2], value.prepend_capped(4, 3))
+    assert_equal([4, 1, 2], value)
+
+    value = [1, 2]
+    assert_equal([4, 1, 2], value.prepend_capped(4, 3))
+    assert_equal([4, 1, 2], value)
+
+    value = [1, 2]
+    assert_equal([4], value.prepend_capped(4, 1))
+    assert_equal([4], value)
   end
 
   it 'gets the previous value in an array' do
